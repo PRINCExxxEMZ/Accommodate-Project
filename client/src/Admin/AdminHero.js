2183418b1ce8eb348a6e1265d2db91c3dcb941f5
Adeoye Prince <adeoyeprinceemz@gmail.com>
2024-11-09 15:07:57 +0100

    Ready_for_Reserve_Function

--- client/src/Admin/AdminHero.js
+++ client/src/Admin/AdminHero.js
@@ -28,8 +28,8 @@ const AdminHero = () => {
                       <div className="w-[316px] top-1/2 transform -translate-y-1/2 absolute z-20 md:w-[450px] mx-5 lg:mx-10 lg:w-[562px]">
                       <h1 className='font-bold text-[25px] text-white lg:text-[35px]'>Accommodation <br/> Seamless Management</h1>
                       <p className='text-white text-[16px] lg:text-[20px]'>Simplify your accomodation management with our innovative solution. With easy room creation and automated fees management </p>
-                      <button className='bg-[#0BA75A] text-white px-5 py-3 rounded-md mt-7 hover:bg-[#1d623f]'><Link to='/createhostel'>Create Hostel</Link></button>
-                      <button className='bg-[#CFA146] text-white px-5 ml-5 py-3 rounded-md mt-7 hover:bg-[#8e6d2c]'><Link to='/createroom'>Create Room</Link></button>
+                      <button className='bg-[#0BA75A] text-white px-5 py-3 rounded-sm mt-7 hover:bg-[#1d623f]'><Link to='/createhostel'>Create Hostel</Link></button>
+                      <button className='bg-[#CFA146] text-white px-5 ml-5 py-3 rounded-sm mt-7 hover:bg-[#8e6d2c]'><Link to='/createroom'>Create Room</Link></button>
                       </div>
                 </section>
                 
